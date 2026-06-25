d06a007f62b8050130bde82a5fe3f45156587752
Mohamed Aymen Akchiche <akchiche.mohamedaymen@gmail.com>
2026-06-25 18:28:15 +0200

    Remove SSL certificate workaround
    Removed SSL workaround for stopwords import.

--- save_stop_wors.py
+++ save_stop_wors.py
@@ -1,15 +1,7 @@
 import json
 import nltk
 from nltk.corpus import stopwords
-import ssl
 
-# Workaround for SSL certificate issue
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
 
 try:
     stop_words = set(stopwords.words('english'))
